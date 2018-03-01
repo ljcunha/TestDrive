fbac9a55743ded2c0b09a606e9d679a6c6fc2fcb
ASGARD\ljcun <ljcunha@gmail.com>
2018-02-28 21:54:17 -0300

    Until BindingContext

--- TestDrive/TestDrive/MainPage.xaml.cs
+++ TestDrive/TestDrive/MainPage.xaml.cs
@@ -9,9 +9,20 @@ namespace TestDrive
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        public List<Veiculo> Veiculos { get; set; }
+
+        public MainPage()
 		{
 			InitializeComponent();
+
+            this.Veiculos = new List<Veiculo>
+            {
+                new Veiculo { Nome = "Azera V6", Preco = 60000},
+                new Veiculo { Nome = "Fiesta 2.0", Preco = 50000},
+                new Veiculo { Nome = "HB20 S", Preco = 40000}
+            };
+
+            this.BindingContext = this;
 		}
 	}
 }
